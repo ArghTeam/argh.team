dd47a2a1307ee36fce4db7909248afd3592947d2
ftroitsky <fedor.troitsky@gmail.com>
2017-12-31 16:59:50 +0300

    deploy production & build production

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
+const production = () => process.env.NODE_ENV === 'production'
+
 module.exports = {
   siteMetadata: {
     title: `Argh Team`,
   },
-  pathPrefix: `/argh.team`,
+  pathPrefix: production ? '/' : '/argh.team',
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
